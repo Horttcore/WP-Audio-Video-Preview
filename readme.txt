a54cbf34e981fa942515df5def87f44aa99cc457
Ralf Hortt <me@horttcore.de>
2013-08-22 12:04:22 +0200

    Initial release
    Signed-off-by: Ralf Hortt <me@horttcore.de>

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== WP Audio/Video Preview ===
+Contributors: Horttcore
+Donate link: http://www.horttcore.de
+Tags: audio, video, preview, admin
+Requires at least: 3.3
+Tested up to: 3.6
+Stable tag: 2.0.2
+
+Redirect a page to an URL
+
+== Description ==
+
+Display an additional column in media library overview
+
+== Installation ==
+
+*   Put the plugin file in your plugin directory and activate it in your WP backend.
+*   Go to your media library
+
+== Screenshots ==
+
+1. Screenshot of the Meta box in the content
+
+== Frequently Asked Questions ==
+
+= I've found a bug, what to do? =
+*   Please give me a shout over github ( https://github.com/Horttcore/WP-Audio-Video-Preview/issues )
+
+== Changelog ==
+
+1.0
+*   First release
---
+++ wp-audio-video-preview.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Plugin Name: WP Audio Preview
+Plugin URI: http://horttcore.de
+Description: Display an additional column in media library overview
+Version: 1.0
+Author: Ralf Hortt
+Author URI: http://horttcore.de
+License: GPL2
+*/
+
+
+/**
+* WP Audio Preview
+*/
+class WP_Audio_Video_Preview
+{
+
+
+	/**
+	 * Constructor
+	 *
+	 * @access public
+	 * @since v1.0
+	 * @author Ralf Hortt
+	 */
+	public function __construct()
+	{
+		add_filter( 'manage_media_columns', array( $this, 'manage_media_columns' ) );
+		add_action( 'manage_media_custom_column', array( $this, 'manage_media_custom_column' ), 10, 2 );
+		load_plugin_textdomain( 'wp-audio-video-preview', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+
+
+	/**
+	 * Add column
+	 *
+	 * @access public
+	 * @param array $columns Columns
+	 * @return array Columns
+	 * @since v1.0
+	 * @author Ralf Hortt
+	 */
+	public function manage_media_columns( array $columns ){
+		$columns['audio_video_preview']  = __( 'Audio/Video Preview', 'wp-audio-video-preview' );
+		return $columns;
+	}
+
+
+
+	/**
+	 * Populate custom columns
+	 *
+	 * @access public
+	 * @param str $column Column
+	 * @param int $post_id Post ID
+	 * @since v1.0
+	 * @author Ralf Hortt
+	 */
+	public function manage_media_custom_column( $column, $post_id ){
+		global $post;
+
+		switch( $column ) :
+
+			case 'audio_video_preview' :
+				if ( FALSE !== strpos($post->post_mime_type, 'audio' )  || FALSE !== strpos($post->post_mime_type, 'video' ) ) :
+					echo apply_filters( 'the_content', $post->guid );
+				endif;
+			break;
+
+		endswitch;
+	}
+
+
+
+}
+
+new WP_Audio_Video_Preview;
